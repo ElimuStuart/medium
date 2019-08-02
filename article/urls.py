150487937b5b0dab7941f69e748465100648b952
ElimuStuart <stuartelimu@gmail.com>
2019-08-02 04:28:47 +0300

    using GenericAPIView

--- article/urls.py
+++ article/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import ArticleView
+from .views import ArticleView, SingleArticleView
 
 app_name = 'article'
 
 urlpatterns = [
     path('articles/', ArticleView.as_view()),
-    path('articles/<int:pk>', ArticleView.as_view()),
+    path('articles/<int:pk>', SingleArticleView.as_view()),
 ]
